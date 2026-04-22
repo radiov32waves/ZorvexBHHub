3ce34aa2b63c4bf8e247cb001e04499b2dd5bfe4
radiov32waves <7li.7hmed0@gmail.com>
2026-04-22 14:44:21 +0300

    Update TheUnknownHub.lua

--- TheUnknownHub.lua
+++ TheUnknownHub.lua
@@ -1,6 +1,6 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "TheUnknownHub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "ZorvexBH Hub⚡", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
 OrionLib:MakeNotification({
 	Name = "Notification!",
@@ -23,9 +23,9 @@ local Section = Tab.Main:AddSection({
 	Name = "Main"
 })
 
-Tab.Main:AddParagraph("My Discord Channel","TheUnknownHub")
+Tab.Main:AddParagraph("My Telegram Channel","ZorvexBHHub")
 
-Tab.Main:AddParagraph("What Is TheUnknownHub Script","TheUnknownHub | by radiov32waves is a Roblox multi-game script hub designed to give players access to features for several popular games such as Blox Fruits, Adopt Me, MM2, Greenville, 99 Night, Grow a Garden, and Steal a Brainrot, all from one clean and simple interface, featuring a main tab with a side navigation menu for easy switching between games, a dark modern UI with open and close controls, and an additional “All Evade Scripts” section that is currently under development, making it a centralized and expanding hub aimed at convenience and multi-game support.")					
+Tab.Main:AddParagraph("What Is ZorvexBHHub Script","ZorvexBHHub | by radiov32waves is a Roblox multi-game script hub designed to give players access to features for several popular games such as Blox Fruits, Adopt Me, MM2, Greenville, 99 Night, Grow a Garden, and Steal a Brainrot, all from one clean and simple interface, featuring a main tab with a side navigation menu for easy switching between games, a dark modern UI with open and close controls, and an additional “All Evade Scripts” section that is currently under development, making it a centralized and expanding hub aimed at convenience and multi-game support.")					
 
 ----------------------------------------------start adoptme scripts------------------------------------
 local Section = Tab.s1:AddSection({
