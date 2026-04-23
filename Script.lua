b73afc613e121b29b9815fc1c5c73f2b3a52debe
radiov32waves <7li.7hmed0@gmail.com>
2026-04-23 15:21:01 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,6 +1,6 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 
--- 1. YOUR KEY LIST
+-- 1. KEY SETTINGS
 local ValidKeys = {
     "9999",
     "8F2R7K9A1Q", "3M5V6N8X2P", "9L1B4C7Z6W", "2S9XPBUI5Z",
@@ -10,11 +10,9 @@ local ValidKeys = {
 
 local EnteredKey = ""
 
--- 2. MAIN HUB FUNCTION
+-- 2. MAIN HUB FUNCTION (YOUR ORIGINAL SCRIPT)
 function LoadScript()
     local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
-    
-    -- Main Script Name matches Key System style + Credit
     local Window = OrionLib:MakeWindow({
         Name = "Key System | ZorvexBH Hub⚡ | by radiov32waves", 
         HidePremium = false, 
@@ -24,7 +22,7 @@ function LoadScript()
 
     OrionLib:MakeNotification({
         Name = "Success!",
-        Content = "Welcome back, radiov32waves",
+        Content = "Welcome to ZorvexBH Hub",
         Image = "rbxassetid://4483345998",
         Time = 10
     })
@@ -36,10 +34,9 @@ function LoadScript()
         s3 = Window:MakeTab({Name = "BloxFruit", Icon = "rbxassetid://4483345998"}),
     }
 
-    ----------------------------------------------main------------------------------------
+    ----------------- MAIN -----------------
     Tab.Main:AddSection({Name = "Information"})
     Tab.Main:AddParagraph("My Telegram Channel","ZorvexBHHub")
-    
     Tab.Main:AddButton({
         Name = "Copy Telegram Link",
         Callback = function()
@@ -48,19 +45,24 @@ function LoadScript()
         end
     })
 
-    ----------------------------------------------adoptme------------------------------------
+    ----------------- ADOPT ME -----------------
     Tab.s1:AddSection({Name = "Adoptme"})
     Tab.s1:AddButton({Name = "TbiGui Script", Callback = function() loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c29af06acdb7a80fb239415fd322efe9.lua"))() end})
     Tab.s1:AddButton({Name = "Clone House Script", Callback = function() loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/051b9ab9e55bc6acf1ead9319bd606ec.lua"))() end})
+    Tab.s1:AddButton({Name = "Free Script", Callback = function() loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/4a873606f2053bf3067ad6da43b5baee.lua"))() end})
 
-    --------------------------------------------greenville------------------------------
+    ----------------- GREENVILLE -----------------
     Tab.s2:AddSection({Name = "Greenville"})
     Tab.s2:AddButton({Name = "Auto Farm", Callback = function() loadstring(game:HttpGet("https://api.xnexus.cc/request/12d2fd60e7a67b48ece6f8fce7167a1d"))() end})
 
-    -----------------------------------------bloxfruit---------------------------------------------
+    ----------------- BLOX FRUIT -----------------
     Tab.s3:AddSection({Name = "BloxFruit"})
     Tab.s3:AddButton({Name = "Redz Hub Script", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Scripts/refs/heads/main/main.luau"))({JoinTeam = "Pirates", Translator = true}) end})
     Tab.s3:AddButton({Name = "GravityHub Script", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-GravityHub/BloxFruit/refs/heads/main/Main.lua"))() end})
+    Tab.s3:AddButton({Name = "Kaitun Script", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-GravityHub/BloxFruit/refs/heads/main/Kaitun.lua"))() end})
+    Tab.s3:AddButton({Name = "Find Fruit Script", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-GravityHub/BloxFruit/refs/heads/main/FindFruit.lua"))() end})
+    Tab.s3:AddButton({Name = "Auto Bounty Script", Callback = function() loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/09eeb45f4cc3ad5a"))() end})
+    Tab.s3:AddButton({Name = "Teddy Hub Boss", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Teddyseetink/Haidepzai/refs/heads/main/TEDDYHUB-FREEMIUM"))() end})
     Tab.s3:AddButton({Name = "BlueXHub Script", Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/Main.lua"))() end})
 
     OrionLib:Init()
@@ -70,7 +72,6 @@ end
 local KeyWindow = OrionLib:MakeWindow({Name = "Key System | ZorvexBH Hub⚡", HidePremium = true, SaveConfig = false})
 
 local HelpTab = KeyWindow:MakeTab({Name = "How to get Key", Icon = "rbxassetid://4483345998"})
-HelpTab:AddParagraph("Step 1", "Join our Telegram channel.")
 HelpTab:AddButton({
     Name = "Copy Telegram Link",
     Callback = function()
@@ -102,7 +103,15 @@ VerifyTab:AddButton({
         end
 
         if success then
-            OrionLib:MakeNotification({Name = "Access Granted", Content = "Loading Hub...", Time = 3})
+            local now = os.date("*t")
+            local hours = 23 - now.hour
+            local mins = 59 - now.min
+
+            OrionLib:MakeNotification({
+                Name = "Access Granted", 
+                Content = "Time left: " .. hours .. "h " .. mins .. "m", 
+                Time = 5
+            })
             task.wait(1)
             OrionLib:Destroy()
             task.wait(1)
